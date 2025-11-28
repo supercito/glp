c278dbd3ea2ee1c014394ba4bb72b7170e80f27d
supercito <supercito21@gmail.com>
2025-11-28 18:26:12 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 # --- Configuración de la página ---
-st.set_page_config(page_title="Calculadora GLP", page_icon="🔥⛽")
+st.set_page_config(page_title="Calculadora GLP", page_icon="⛽")
 
 # --- Estilos CSS personalizados ---
 st.markdown("""
